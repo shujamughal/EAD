660189adeba85754cd1328b5cfc546dc1b61253c
shujamughal <shujamughal@gmail.com>
2020-09-11 11:31:39 +0500

    4th lecture

---
+++ EAD_Lec4/EAD_Lec4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace EAD_Lec4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+
+            Console.WriteLine(args[0]);
+            Console.WriteLine(args[1]);
+            Console.WriteLine(args[2]);
+            //int numberOfApples = 12;
+            //decimal pricePerApple = 0.35M;
+
+            //Console.WriteLine(numberOfApples + " apples costs "+ 
+            //    (numberOfApples*pricePerApple));
+
+            ////Formatting output using interpolated strings.
+
+            //Console.WriteLine($"{numberOfApples} apples costs {numberOfApples*pricePerApple:C}");
+
+            ////using numbered positional arguments
+            //Console.WriteLine(
+            //    format:"{0} apples costs {1:C}",
+            //    arg0:numberOfApples,
+            //    arg1:pricePerApple*numberOfApples
+            //    );
+
+            //string formattedString = string.Format(
+            //    format: "{0} apples costs {1:C}",
+            //    arg0: numberOfApples,
+            //    arg1: pricePerApple * numberOfApples
+            //    );
+
+            //Console.WriteLine(formattedString);
+
+            ////ReadKey
+            //Console.Write("Press any key combination: ");
+            //ConsoleKeyInfo key = Console.ReadKey();
+            //Console.WriteLine();
+            //Console.WriteLine(
+            //    format:"key: {0}, Char: {1}, Modifier: {2}",
+            //    arg0: key.Key,
+            //    arg1:key.KeyChar,
+            //    arg2:key.Modifiers
+            //    );
+
+
+
+        }
+    }
+}
