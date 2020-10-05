2073d1e34fb0afa00d44425e021f3a9d42ee60be
shujamughal <shujamughal@gmail.com>
2020-10-05 13:13:57 +0500

    Lecture10

---
+++ Lecture10/Lecture10/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture10
+{
+    /*class TextAndNumber {
+        public string Text;
+        public int Number;
+    }*/
+
+    class Person
+    {
+        public string Name;
+        public int Age;
+        public string Country;
+
+        public void PrintPersonDetails() {
+            Console.WriteLine($"Person Name is : {this.Name}" +
+                $"  and age is {this.Age}    ");
+        }
+
+        public string GetPersonName() {
+            return $"My Name is {this.Name}";
+        }
+
+        public (string MyName, int MyAge, string MyCountry) GetPersonDetails() {
+            return (this.Name, this.Age, this.Country);
+        }
+
+       /* public TextAndNumber GetPersonNameAndAge() {
+            return new TextAndNumber { Text = this.Name, Number = this.Age };
+        }*/
+
+    }
+}
---
+++ Lecture10/Lecture10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lecture10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person {Age= 12, Name= "Ali", Country= "Pakistan" };
+            //p.PrintPersonDetails();
+            //Console.WriteLine(p.GetPersonName());
+            //Console.WriteLine("name is "+ p.GetPersonNameAndAge().Text + "and age is "+
+            //                  p.GetPersonNameAndAge().Number  );
+            (string name, int age, string country)  = p.GetPersonDetails();
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(country);
+
+        }
+    }
+}
