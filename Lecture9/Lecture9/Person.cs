a041fdbaf0fabf29f81394b8dd57dbaf4efe2a4f
shujamughal <shujamughal@gmail.com>
2020-10-05 12:28:17 +0500

    Lecture9

---
+++ Lecture9/Lecture9/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture9
+{
+    public class Person : System.Object
+    {
+        public string Name;
+        public int Age;
+        public static string Country = "Pakistan";
+        public const string HomePlanet = "Earth" ;
+        public readonly string HomePlanet2;
+          
+
+        public Person() {
+            Name = default;
+            Age = default;
+            HomePlanet2 = "Moon";
+        }
+
+        public Person(string name, int age) {
+            this.Name = name;
+            this.Age = age;
+            HomePlanet2 = "Moon";
+
+        }
+    }
+}
---
+++ Lecture9/Lecture9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lecture9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personObj = new Person("ali", 12);
+            Console.WriteLine(personObj.Name);
+            Console.WriteLine(personObj.Age);
+            Console.WriteLine(Person.Country);
+            Console.WriteLine(Person.HomePlanet);
+            Console.WriteLine(personObj.HomePlanet2);
+
+            Person p2 = new Person { Name="mustafa", Age=20};
+
+
+
+
+        }
+    }
+}
