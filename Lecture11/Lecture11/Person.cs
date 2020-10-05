28284d905adcbf0e9bdb9528b9644f59f1be01c1
shujamughal <shujamughal@gmail.com>
2020-10-05 13:55:41 +0500

    Lecture11

---
+++ Lecture11/Lecture11/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace Lecture11
+{
+    class Person
+    {
+        public string Name;
+        public int Age;
+        public string Country;
+
+        public void SayHello() {
+            Console.WriteLine($"My name is {this.Name}");
+        }
+
+        public void SayHello(string inputName)
+        {
+            Console.WriteLine($"My name is {this.Name} and input name is {inputName}");
+        }
+
+        public void SayHello(string inputName, int myValue)
+        {
+            Console.WriteLine($"My name is {this.Name} and input name is {inputName}");
+        }
+
+        public void MyMethod(string name="shuja", int someValue=5, double someOtherValue=2.5) {
+            Console.WriteLine($"name is {name} and int value is {someValue} " +
+                $" and double is {someOtherValue}");
+        }
+
+        public int TotalMarks(params int[] data) {
+            int result = 0;
+            //logic to add these numbers
+            return result;
+        }
+
+        public void PassingParamters(int x, ref int y, out int z) {
+            z = 99;
+            x++;
+            y++;
+            z++;
+
+        }
+    }
+}
---
+++ Lecture11/Lecture11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lecture11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person { Name="Ali", Age=12, Country= "Pakistan"};
+            //p.SayHello("shuja", 12);
+            //p.MyMethod("ali",123,85.69);
+            //p.TotalMarks(1);
+            //p.TotalMarks(1,3);
+            //p.TotalMarks(1,4,5,6);
+            //p.TotalMarks(1,2,3,4,5,67,8,9);
+            //p.TotalMarks(1, 2, 3, 4, 5, 67, 8, 9,23);
+            int a = 10;
+            int b = 20;
+            
+            Console.WriteLine($"before passing a ={a}, b = {b}");
+            p.PassingParamters(a, ref b,  out int c);
+            Console.WriteLine($"after passing a ={a}, b = {b} and c={c}");
+        }
+    }
+}
