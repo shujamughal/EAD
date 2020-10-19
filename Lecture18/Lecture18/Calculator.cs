2f7f45e484bf9522522452bff8be3f73c157b180
shujamughal <shujamughal@gmail.com>
2020-10-19 18:23:14 +0500

    Lecture18

---
+++ Lecture18/Lecture18/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture18
+{
+    internal class Calculator
+    {
+       static public int Add(int a, int b) {
+            return a + b;
+
+        }
+
+        static public int Subtract(int a, int b) {
+            return a - b;
+
+        }
+    }
+}
---
+++ Lecture18/Lecture18/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Lecture18
+{
+    delegate void Delegate1();
+    delegate void Delegate2(string s);
+    delegate string Delegate3(string s);
+    delegate int MathOp(int x, int y);
+    class Program
+    {
+        static int PerformOperation(MathOp op, int x, int y) {
+            return op(x, y);
+
+        }
+        static string Display3(string s) {
+            Console.WriteLine(s);
+            return s;
+        }
+        static void Display() {
+            Console.WriteLine("Hello World!...");
+        }
+        static void Display4()
+        {
+            Console.WriteLine("Hello World!...from display 4");
+        }
+        static void Display2(string s)
+        {
+            Console.WriteLine(s);
+
+        }
+          
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(PerformOperation(Calculator.Add, 4, 6));
+            Console.WriteLine(PerformOperation(Calculator.Subtract, 4, 6));
+            string input = Console.ReadLine();
+            int i = System.Convert.ToInt32(input);
+
+            Delegate1 d1 = null;
+
+            if (i == 0)
+            {
+                d1 = Display;
+            }
+            else {
+                d1 = Display4;
+            }
+            d1.Invoke();
+            d1();
+            //Delegate2 d2 = new Delegate2(Display2);
+            Delegate2 d2 = Display2;
+            d2("AOA Pakistan...");
+
+            Delegate3 d3 = Display3;
+            string s = d3("Some text");
+        }
+    }
+}
