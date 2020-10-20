e1b35e15fb81bcf2d967e0606af20c088d2b3407
shujamughal <shujamughal@gmail.com>
2020-10-20 16:51:40 +0500

    Lecture 19

---
+++ Lecture19/Lecture19/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture19
+{
+    internal class Calculator
+    {
+        static internal int Add(int a, int b) {
+            int result = a + b;
+            Console.WriteLine($"Add Method result:{result}");
+            return result;
+        }
+        static internal int Subtract(int a, int b) {
+            int result = a - b;
+            Console.WriteLine($"Subtract Method result: {result}");
+            return result;
+        }
+        static internal int Multiply(int a, int b) {
+            int result = a * b;
+            Console.WriteLine($"Multiply Method result: {result}");
+            return result;
+            
+        }
+    }
+}
---
+++ Lecture19/Lecture19/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Lecture19
+{
+    delegate int MathOperation(int x, int y);
+    delegate void MyDelegate();
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Delegate Example---");
+
+            MathOperation op = delegate (int x, int y) {
+                Console.WriteLine("Add");
+                return x + y;
+            };
+            //lambda Statement
+            op +=  ( x,  y) => {
+                Console.WriteLine("Subtract");
+                return x - y;
+            };
+
+            //Lambda Expression
+            op += (x, y) => x * y;
+
+            MyDelegate d1 = () => Console.WriteLine("AOA...");
+
+            d1();
+            /*MyDelegate d1 = delegate () {
+                Console.WriteLine("I am in anonymous method");
+            };
+            d1();*/
+            Console.WriteLine(op(2,3));
+            /*MathOperation op = Calculator.Add;
+            //int result = op(6, 3);
+            //Console.WriteLine($"result : {result}");
+            op += Calculator.Subtract;
+            op += Calculator.Multiply;
+
+            int result = op(5, 4);
+            Console.WriteLine(result);
+
+            op -= Calculator.Subtract;
+
+             result = op(5, 4);
+            Console.WriteLine(result);*/
+            
+
+
+
+
+
+
+
+        }
+    }
+}
