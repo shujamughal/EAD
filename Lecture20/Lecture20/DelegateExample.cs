4993dd94152926ff4d4797e669aba9a5e9ba6a67
shujamughal <shujamughal@gmail.com>
2020-10-21 12:56:51 +0500

    lecture 20

---
+++ Lecture20/Lecture20/DelegateExample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*
+Func is a generic delegate that encapsulates a method that can accept parameters 
+and return some value.
+
+There are 17 Func delegates defined in the class library. 
+The first delegate cannot accept parameters and  can only return value
+Other Func delegate can take upto 16 parameters and one return value. 
+The last parameter is always the return type.
+
+ */
+namespace Lecture20
+{
+    class DelegateExample
+    {
+        public static string Sum(int a, int b)
+        {
+            string str = string.Format("Sum: {0}", (a + b));
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            Func<int, int, string> func = Sum;
+
+            string result = func(2, 4);
+            Console.WriteLine(result); //Print Sum: 6
+        }
+    }
+}
---
+++ Lecture20/Lecture20/Program.cs
@@ -0,0 +1,61 @@
+﻿/* using System;
+/*
+Microsoft introduced some pre-built delegates so that we don't have to declare delegates
+every time. Action is one of the pre-built delegates.
+
+Action in C# represents a delegate that has void return type and optional parameters. 
+There are two variants of Action delegate.
+
+1-Action
+2-Action<in T>
+
+1- Action
+First variant is non-generic delegate that takes no parameters and has void return type. 
+In this Action delegate, we can store only those methods which has no parameters and 
+void return type.
+
+2- Action<in T>
+
+The second variant is a family of 16 generic delegates.
+These delegates can take upto 16 parameters and all have void return type.
+ //
+
+
+namespace Lecture20
+{
+    class Program
+    {
+        public static void DoWorkWithOneParameter(int arg)
+        {
+            Console.WriteLine(arg);
+        }
+
+        public static void DoWorkWithTwoParameters(int arg1, int arg2)
+        {
+            Console.WriteLine(arg1 + "-" + arg2);
+        }
+
+        public static void DoWorkWithThreeParameters(int arg1, int arg2, int arg3)
+        {
+            Console.WriteLine(arg1 + "-" + arg2 + "-" + arg3);
+        }
+        public static void DoWork()
+        {
+            Console.WriteLine("Hi, I am doing work.");
+        }
+        static void Main(string[] args)
+        {
+            Action doWorkAction = new Action(DoWork);
+            doWorkAction(); //Print "Hi, I am doing work."
+
+            Action<int> firstAction = DoWorkWithOneParameter;
+            Action<int, int> secondAction = DoWorkWithTwoParameters;
+            Action<int, int, int> thirdAction = DoWorkWithThreeParameters;
+
+            firstAction(1); // Print 1
+            secondAction(1, 2); // Print 1-2
+            thirdAction(1, 2, 3); //Print 1-2-3
+
+        }
+    }
+}*/
