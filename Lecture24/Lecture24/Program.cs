99c5d9d82e9fc7637031b49e9ffdd2bc435d498c
shujamughal <shujamughal@gmail.com>
2020-10-27 14:16:53 +0500

    events in c#. 24th lecture

---
+++ Lecture24/Lecture24/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+
+namespace Lecture24
+{
+    delegate void MyEventHandler();
+    
+    //publisher
+    internal class MyButton {
+
+        public event MyEventHandler click;
+
+        public void OnClick() {
+
+            //click.Invoke();
+            click();
+        }
+
+    }
+
+    internal class MySubscriber {
+
+
+       static public void HandleEvent() {
+
+            Console.WriteLine("Click event occured.");
+        }
+    
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyButton button = new MyButton();
+            button.click += MySubscriber.HandleEvent;
+
+            button.click += delegate ()
+            {
+                Console.WriteLine("click event second handler");
+            };
+
+            button.OnClick();
+        }
+    }
+}
