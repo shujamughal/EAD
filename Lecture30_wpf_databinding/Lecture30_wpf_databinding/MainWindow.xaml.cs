785914d8376e1370140f5655266862c485bbb4d6
shujamughal <shujamughal@gmail.com>
2020-11-03 13:40:05 +0500

    wpf_data_binding

---
+++ Lecture30_wpf_databinding/Lecture30_wpf_databinding/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture30_wpf_databinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Binding b = new Binding("Text");
+            b.Source = txtSource;
+            b.Mode = BindingMode.TwoWay;
+            b.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            txtTarget.SetBinding(TextBox.TextProperty,b);
+
+        }
+    }
+}
