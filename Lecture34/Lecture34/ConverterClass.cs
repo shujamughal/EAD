8e14172cd8eb20b6cf12a99cb76b9d2278c20d5a
shujamughal <shujamughal@gmail.com>
2020-11-09 16:02:48 +0500

    IMultiValueConverter

---
+++ Lecture34/Lecture34/ConverterClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Lecture34
+{
+    class ConverterClass : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Concat(values[0],",",values[1]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return (value as string).Split(',');
+        }
+    }
+}
