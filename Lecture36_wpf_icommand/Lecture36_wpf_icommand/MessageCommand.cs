2d82a856dec8cd4ab14e024d80c7ed1a05c6d942
shujamughal <shujamughal@gmail.com>
2020-11-11 13:21:21 +0500

    wpf ICommand

---
+++ Lecture36_wpf_icommand/Lecture36_wpf_icommand/MessageCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Lecture36_wpf_icommand
+{
+    class MessageCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action methodExected;
+
+        public MessageCommand(Action method)
+        {
+            this.methodExected = method;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            methodExected.Invoke();
+        }
+    }
+}
