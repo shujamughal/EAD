1e23f23c61a6e7f10f7c872346aeea70530fe1ab
shujamughal <shujamughal@gmail.com>
2020-11-13 14:34:49 +0500

    wpf command paramter and event handling

---
+++ Lecture37_wpf_icommand/Lecture36_wpf_icommand/MessageCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Lecture36_wpf_icommand
+{
+    class MessageCommand : ICommand
+    {
+        //public event EventHandler CanExecuteChanged;
+        public event EventHandler CanExecuteChanged {
+            add {
+                CommandManager.RequerySuggested += value;
+            }
+            remove {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+        
+        private MessageViewModel messagevm;
+        public MessageCommand(MessageViewModel vm)
+        {
+            this.messagevm = vm;
+        }
+        public bool CanExecute(object parameter)
+        {
+
+            return messagevm.canExecute(parameter as string);
+        }
+
+        public void Execute(object parameter)
+        {
+            messagevm.DisplayMessage(parameter as string);
+        }
+        
+    }
+}
---
+++ Lecture37_wpf_icommand/Lecture36_wpf_icommand/MessageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Lecture36_wpf_icommand
+{
+    class MessageViewModel
+    {
+        
+        public MessageCommand cmd { get; set; }
+        public MessageViewModel()
+        {
+            cmd = new MessageCommand(this);
+        }
+
+        public bool canExecute(string m) {
+
+            if (string.IsNullOrEmpty(m))
+            {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        public void DisplayMessage(string m) {
+           
+            MessageBox.Show(m);
+            
+        }
+    }
+}
