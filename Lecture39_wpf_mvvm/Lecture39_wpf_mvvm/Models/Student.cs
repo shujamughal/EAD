e1069224ba377c73c8bca222d31eb64d193aa53f
shujamughal <shujamughal@gmail.com>
2020-11-19 12:39:47 +0500

    39_mvvm_wpf

---
+++ Lecture39_wpf_mvvm/Lecture39_wpf_mvvm/Models/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+namespace Lecture39_wpf_mvvm.Models
+{
+    class Student : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propName) {
+
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        private int id;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged("ID"); }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged("Name"); }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; OnPropertyChanged("Age"); }
+        }
+
+
+
+
+    }
+}
---
+++ Lecture39_wpf_mvvm/Lecture39_wpf_mvvm/Models/StudentService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+namespace Lecture39_wpf_mvvm.Models
+{
+    class StudentService
+    {
+        ObservableCollection<Student> studentList;
+
+        public StudentService()
+        {
+            studentList = new ObservableCollection<Student>() { 
+            new Student { ID=1, Name="Ibrahim", Age=2}
+            };
+        }
+        public void AddStudent(Student s) {
+            studentList.Add(s);
+        }
+        public ObservableCollection<Student> GetAllStudents() {
+            return studentList;
+        }
+    }
+}
