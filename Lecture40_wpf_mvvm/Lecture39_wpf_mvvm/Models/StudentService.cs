f13ed696d2fab3fd4ee7e21962e3ed3a60b4e176
shujamughal <shujamughal@gmail.com>
2020-11-19 15:24:02 +0500

    rename lecture 31

---
+++ Lecture40_wpf_mvvm/Lecture39_wpf_mvvm/Models/StudentService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+namespace Lecture39_wpf_mvvm.Models
+{
+    class StudentService
+    {
+        ObservableCollection<Student> studentList;
+
+        public StudentService()
+        {
+            studentList = new ObservableCollection<Student>() { 
+            new Student { ID=1, Name="Ibrahim", Age=2}
+            };
+        }
+        public void AddStudent(Student s) {
+            studentList.Add(s);
+        }
+        public ObservableCollection<Student> GetAllStudents() {
+            return studentList;
+        }
+        public void RemoveStudent(Student s)
+        {
+            studentList.Remove(s);
+        }
+        public bool UpdateStudent(Student s)
+        {
+
+            bool isUpdated = false;
+
+            for (int i = 0; i < studentList.Count; i++)
+            {
+                if (studentList[i].ID == s.ID)
+                {
+                    studentList[i].Name = s.Name;
+                    studentList[i].Age = s.Age;
+                    isUpdated = true;
+                    break;
+                }
+            }
+
+            return isUpdated;
+        }
+        public Student SearchStudent(int ID)
+        {
+
+            Student s = null;
+            for (int i = 0; i < studentList.Count; i++)
+            {
+                if (studentList[i].ID == ID)
+                {
+                    s = studentList[i];
+                    break;
+                }
+            }
+            return s;
+        }
+    }
+}
---
+++ Lecture40_wpf_mvvm/Lecture39_wpf_mvvm/ViewModels/StudentViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Lecture39_wpf_mvvm.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using Lecture39_wpf_mvvm.Commands;
+using System.Windows;
+
+namespace Lecture39_wpf_mvvm.ViewModels
+{
+    class StudentViewModel : DependencyObject
+    {
+        public Student NewStudent { get; set; }
+        public ObservableCollection<Student>  Students   { get; set; }
+        public DelegateCommand AddCommand { get; set; }
+        public DelegateCommand UpdateCommand { get; set; }
+        public DelegateCommand RemoveCommand { get; set; }
+
+        StudentService studentService;
+        public StudentViewModel()
+        {
+            studentService = new StudentService();
+            NewStudent = new Student();
+            Students = studentService.GetAllStudents();
+            AddCommand = new DelegateCommand(Add, canAdd);
+            RemoveCommand = new DelegateCommand(Remove, canRemove);
+        }
+
+        public void Add(object o) {
+            studentService.AddStudent(NewStudent);
+        }
+
+        public void Remove(object o) {
+            if (o != null)
+            {
+                studentService.RemoveStudent(o as Student);
+            }
+        }
+        public bool canRemove(object o) {
+            return true;
+            /*if (o != null)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }*/
+        }
+        public bool canAdd(object o) {
+
+            if (string.IsNullOrEmpty(NewStudent.ID.ToString()) ||
+               string.IsNullOrEmpty(NewStudent.Name) ||
+               string.IsNullOrEmpty(NewStudent.Age.ToString()))
+            {
+                return false;
+            }
+            else {
+                return true;
+            }
+        
+        }
+    }
+}
