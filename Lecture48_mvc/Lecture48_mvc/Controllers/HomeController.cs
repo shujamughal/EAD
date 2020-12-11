8ad012624cbbefca699803199a406484c65e1f51
shujamughal <shujamughal@gmail.com>
2020-12-11 14:41:49 +0500

    asp.net mvc 48

---
+++ Lecture48_mvc/Lecture48_mvc/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lecture48_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() {
+            string myData = "This is my dummy data";
+            return View("Index",myData);
+        }
+
+        public ViewResult Student()
+        {
+
+            return View();
+        }
+    }
+}
