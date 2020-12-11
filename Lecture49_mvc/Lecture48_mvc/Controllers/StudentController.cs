17c0f6f47f858bff5bd116edbc5fef888f01edf4
shujamughal <shujamughal@gmail.com>
2020-12-11 15:52:30 +0500

    mvc add 49

---
+++ Lecture49_mvc/Lecture48_mvc/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lecture48_mvc.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ViewResult StudentForm() {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult StudentForm(string name, string age, string cgpa, string semester)
+        {
+            return View();
+        }
+    }
+}
---
+++ Lecture49_mvc/Lecture48_mvc/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lecture48_mvc.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public float CGPA { get; set; }
+        public string Semester { get; set; }
+    }
+}
