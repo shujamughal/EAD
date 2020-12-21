be4c0c6f73a48712ea4c7e08fe89aceba210fe37
shujamughal <shujamughal@gmail.com>
2020-12-21 15:39:29 +0500

    asp mvc mocel validation 51

---
+++ Lecture51_mvc -model validation/Lecture48_mvc/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lecture48_mvc.Models;
+namespace Lecture48_mvc.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ViewResult StudentForm() {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult StudentForm(Student s)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                StudentRepository.AddStudent(s);
+                return View("Thanks", s);
+            }
+            else {
+                ModelState.AddModelError(String.Empty, "Please enter correct data");
+                return View();
+            }
+        }
+
+        public ViewResult ListStudents() {
+            return View(StudentRepository.students);
+        }
+    }
+}
---
+++ Lecture51_mvc -model validation/Lecture48_mvc/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Lecture48_mvc.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage ="Please enter name")]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="please enter your age")]
+        [Range(1,100)]
+        public int? Age { get; set; }
+        [Required(ErrorMessage ="enter cgpa")]
+        public float? CGPA { get; set; }
+        [Required(ErrorMessage ="enter your semester")]
+        public string Semester { get; set; }
+    }
+}
