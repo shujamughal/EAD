380a023b19fc878626c3dc2537a4301bfe1038f1
shujamughal <shujamughal@gmail.com>
2020-12-31 19:01:29 +0500

    sessions in mvc 57

---
+++ Lecture_57_session/Lecture_56/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Lecture_56.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lecture_56.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string data = String.Empty;
+            if (HttpContext.Session.Keys.Contains("first_request"))
+            {
+                string firstVisitedDateTime = HttpContext.Session.GetString("first_request");
+                data = "Welcome back " + firstVisitedDateTime;
+
+            }
+            else
+            {
+                
+                data = "you visited first time";
+                HttpContext.Session.SetString("first_request", System.DateTime.Now.ToString());
+            }
+            return View("index", data);
+        }
+
+        public IActionResult Remove()
+        {
+            HttpContext.Session.Remove("first_request");
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lecture_57_session/Lecture_56/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lecture_56.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
