4f885e8eb9eee4e6a6f40eaaedba6c97e1249e90
shujamughal <shujamughal@gmail.com>
2021-01-14 15:06:03 +0500

    58

---
+++ Lecture58_webapi/Lecture58_webapi/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lecture58_webapi.Models;
+
+namespace Lecture58_webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        StudentRepository repo;
+        
+        public StudentController()
+        {
+            repo = new StudentRepository();
+        }
+
+
+        [HttpGet]
+        public List<Student> Get()
+        {
+            return repo.GetAllStudent();
+        }
+
+        [HttpGet]
+        //[Route("[action]")]
+        [Route("myStudents")]
+        [Route("myStudents2")]
+        [Route("api/controller/myStudents")]
+
+        public List<Student> GetAllofStudents()
+        {
+            return repo.GetAllStudent();
+        }
+    }
+}
---
+++ Lecture58_webapi/Lecture58_webapi/Models/StudentRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lecture58_webapi.Models
+{
+    public class StudentRepository
+    {
+        public List<Student> GetAllStudent() {
+
+            List<Student> students = new List<Student>() { 
+            new Student { Id=1,Name="Ibrahim", Age=3},
+            new Student { Id=2,Name="Alishba", Age=4},
+            new Student { Id=3,Name="Waheed", Age=5},
+            };
+
+            return students;
+        }
+    }
+}
