7dcb1dccf90d35afe4e07f797cec9dfb28dfd77e
shujamughal <shujamughal@gmail.com>
2021-01-18 16:26:30 +0500

    59 crud web api

---
+++ 59_webservice/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        StudentRepository repo;
+        public StudentController()
+        {
+            repo = new StudentRepository();
+        }
+
+        [HttpGet]
+        [Route("abc")]
+        public List<Student> Get()
+        {
+            return repo.GetAllStudents();
+        }
+
+        [HttpGet]
+        //[HttpGet("{id}")]
+        [Route("find/{id}")]
+        public Student GetById(int id)
+        {
+            return repo.GetStudentById(id);
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public void Add(Student s)
+        {
+            repo.AddNewStudent(s);
+        }
+
+        [HttpPut("{id}")]
+        public void Update(Student s, int id)
+        {
+            //you need to call a method to update student based on id
+        }
+
+        [HttpDelete]
+        [Route("delStd/{id}")]
+
+        public void DeleteStudent(int id)
+        {
+            //you need to call a method to delete student based on id
+        }
+    }
+}
---
+++ 59_webservice/WebApplication1/Models/StudentRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class StudentRepository
+    {
+        List<Student> students;
+        public StudentRepository()
+        {
+            students = new List<Student>() {
+            new Student { Id=1,Name="Ibrahim", Age=3},
+            new Student { Id=2,Name="Alishba", Age=4},
+            new Student { Id=3,Name="Waheed", Age=5},
+            };
+        }
+        public List<Student> GetAllStudents()
+        {
+             
+           return students;
+        }
+
+        public Student GetStudentById(int id) {
+
+            return students.Find(s=> s.Id==id);
+        
+        }
+
+        public void AddNewStudent(Student s) {
+            students.Add(s);
+        }
+
+
+       
+    }
+
+   
+
+
+}
