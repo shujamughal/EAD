20c495ed53b91d7fb343448b10afa35c3f5f1b80
shujamughal <shujamughal@gmail.com>
2021-04-20 16:29:17 +0500

    linq

---
+++ 63_LINQ_Part2/63_LINQ_Part2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+namespace _63_LINQ_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new string[] { "Mustafa", "Shuja", "Zia", "Ibrahim",
+                    "Ali", "Sabir","Azaz" };
+
+            var query = names.Where(NameLongerThanFour);
+
+            foreach (var n in query) {
+                Console.WriteLine(n);
+            }
+        }
+        static bool NameLongerThanFour(string name)
+        {
+            return name.Length > 4;
+        }
+    }
+}
