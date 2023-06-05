a8ea2a8eba7d37ebbabfb4dd239a38ff983b2ff3
Dr. Shuja ur Rehman Baig <shujamughal@gmail.com>
2023-06-05 09:42:20 +0500

    adding signalR examples

---
+++ 73_SignalR/SignalRBasics/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRBasics.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage( string m)
+        {
+            //await Clients.All.SendAsync("ReceiveMessage", m);
+            //await Clients.Caller.SendAsync("ReceiveMessage", m);
+            await Clients.Others.SendAsync("ReceiveMessage", m);
+        }
+    }
+    
+    
+}
